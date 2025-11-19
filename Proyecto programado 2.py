b30b6c6342c09178d87a8ed4378632e956e63a5a
TheProgramer-05 <s.morag0505@gmail.com>
2025-11-19 12:50:55 -0600

    Muros agregados a carpeta

--- Proyecto programado 2.py
+++ Proyecto programado 2.py
@@ -7,18 +7,10 @@ from tkinter import messagebox #Iconos posibles: 'error', 'info', 'question', 'w
 import time
 import Clases.camino as camino
 import Clases.lianas as lianas
+import Clases.tuneles as tuneles
+import Clases.muros as muros
 #CLASES DE LOS CAMINOS ==========================================================================
 
-
-class Muros:
-    def __init__(self):
-        self.tipo = "Muros"
-        
-    def permitir_nadie(self): #Esta f tal vez sobre. Pero mejor ponerla por si acaso
-        """
-        * Nadie puede pasar. Por ende, false
-        """
-        return False
 
 #Lista global de jugadores
 user_jugadores = []
